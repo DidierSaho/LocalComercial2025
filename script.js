2454e3f9d7ae0921c86a966824676d8037bfcb12
Didier Saho <didiersahonero@MacBook-Pro-de-Didier.local>
2025-11-15 18:04:00 -0300

    .

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const storeData = {
     1: {
-        name: 'Artesanías Sakura',
-        description: 'Descubre auténticas artesanías japonesas y souvenirs tradicionales con diseños únicos.',
+        name: 'Perfumeria',
+        description: 'Descubri distintos aromas que te haran sentirte renovad@.',
         details: 'Horario: 10:00 AM - 8:00 PM | Tel: (555) 001-1000'
     },
     2: {
@@ -10,18 +10,18 @@ const storeData = {
         details: 'Horario: 9:00 AM - 9:00 PM | Tel: (555) 001-2000'
     },
     3: {
-        name: 'Tienda de Ropa',
-        description: 'Colecciones modernas con inspiración en la moda japonesa contemporánea.',
+        name: 'Tienda de Ropa ZARA',
+        description: 'Colecciones modernas con inspiración y estilo .',
         details: 'Horario: 10:00 AM - 8:00 PM | Tel: (555) 001-3000'
     },
     4: {
-        name: 'Dulcería',
-        description: 'Postres tradicionales japoneses y dulces artesanales únicos para disfrutar.',
+        name: 'Tienda de accesorios y guarderia de mascotas - 4Patas',
+        description: 'Todo lo necesario para tu mascota.',
         details: 'Horario: 10:00 AM - 8:00 PM | Tel: (555) 001-4000'
     },
     5: {
-        name: 'Joyería Premium',
-        description: 'Joyería fina con diseños inspirados en la tradición oriental y lujo contemporáneo.',
+        name: 'Patio de comidas',
+        description: 'Comidas frias,calientes para llevar o comer en el lugar.',
         details: 'Horario: 11:00 AM - 7:00 PM | Tel: (555) 001-5000'
     }
 };
